eb9726897f9675c116ee02b4c55545546398c57f
Chistik Andrey <chistik.ae@gmail.com>
2024-03-25 21:57:42 +0300

    Закончили этап авторизации

--- src/inputBlock/InputBlock.js
+++ src/inputBlock/InputBlock.js
@@ -6,7 +6,8 @@ export default function InputBlock() {
     const [userName, setUserName] = useState('');
     const [userPassword, setUserPassword] = useState('');
     const [userRole, setUserRole] = useState('');
-    const [userAuth, setUserAuth] = useState('');
+    const [userAuth, setUserAuth] = useState(false);
+
     const userInfo = {
         login: userName,
         pass: userPassword,
@@ -14,36 +15,33 @@ export default function InputBlock() {
         auth: userAuth
     }
 
-    function showUserInfo() {
-        if (userName === 'andrey@vtb.ru') {
-            if (userPassword === 'adminPassword') {
-                setUserRole('admin');
-                setUserAuth('true')
-            } else {
-                setUserRole('');
-                setUserAuth('false')
-            }
+    async function checkUser() {
+        if (userName === 'andrey@vtb.ru' && userPassword === 'adminPassword') {
+            setUserRole('admin');
+            setUserAuth(true)
         } else {
-            if (userName === 'sasha@vtb.ru') {
-                if (userPassword === 'userPassword') {
-                    setUserAuth('true');
-                    setUserRole('default user')
-                } else {
-                    setUserRole('');
-                    setUserAuth('false')
-                }
+            if (userName === 'sasha@vtb.ru' && userPassword === 'userPassword') {
+                setUserAuth(true);
+                setUserRole('default user')
             }
         }
+    }
+
+    function showUserInfo() {
         console.log(JSON.stringify(userInfo, null, 2))
     }
+
 
     return (
         <div className='inputBlock'>
             Авторизация
             <input type='email' placeholder='User Name' id='email' value={userName} onChange={(event) => setUserName(event.target.value)} required />
             <input type='password' placeholder='Password' id='password' value={userPassword} onChange={(event) => setUserPassword(event.target.value)} required />
-            <button className='inputButtom' onClick={() => showUserInfo()} >
-                click me
+            <button className='inputButtom' onClick={() => checkUser()} >
+                проверить
+            </button>
+            <button disabled={!userAuth} className='inputButtom' onClick={() => showUserInfo()} >
+                войти
             </button>
         </div>
     )
